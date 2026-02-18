229029b621a2251504e218a759ab98d43a7a0fa3
Uninkables <jtb2854@gmail.com>
2026-02-18 11:37:50 -0500

    Update character-renderer.js

--- character-renderer.js
+++ character-renderer.js
@@ -524,7 +524,7 @@ const testCard = {
     illustrators: ["Matthew Robert Davies"],
     collector_number: "67",
     lang: "en",
-    set: { code: "11" }
+    set: { code: "10" }
 };
 
 loadCard(testCard);
