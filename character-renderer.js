ebf77ce09e52827198d4061089d88690e3b4370d
Uninkables <jtb2854@gmail.com>
2026-02-19 21:57:44 -0500

    Update character-renderer.js

--- character-renderer.js
+++ character-renderer.js
@@ -474,6 +474,7 @@ function renderCardText(svgRoot, card) {
     
             if (isItalic) {
                 tspan.setAttribute("font-style", "italic");
+                tspan.setAttribute("font-weight", "500");
             }
     
             textEl.appendChild(tspan);
