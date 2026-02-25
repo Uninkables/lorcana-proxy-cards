32e00322d04f52f797cd38ba595a3158c61219cd
Uninkables <jtb2854@gmail.com>
2026-02-24 21:16:04 -0500

    Update character-renderer.js

--- character-renderer.js
+++ character-renderer.js
@@ -507,7 +507,7 @@ const testCard = {
     lore: 2,
     rarity: "Uncommon",
     classifications: ["Storyborn", "Hero", "Princess"],
-    text: "SHADOW POWER When you play this character, you may give chosen character Challenger +2 and Resist +2 until the start of your next turn. (They get +2 {S} while challenging. Damage dealt to them is reduced by 2.)\nETERNAL NIGHT Your Gargoyle characters lose the Stone by Day ability.",
+    text: "SHADOW POWER When you play this character, you may give chosen character Challenger +2 and Resist +2 until the start of {IW} your next turn. (They get +2 {S} while challenging. Damage dealt to them is reduced by 2.)\nETERNAL NIGHT Your Gargoyle characters lose the Stone by Day ability.",
     flavor_text: "",
     keywords: ["Challenger", "Resist"],
     illustrators: ["Matthew Robert Davies"],
