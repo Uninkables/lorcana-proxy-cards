6a374824f9933486b6c0b7dc8cb9055c1427f666
Uninkables <jtb2854@gmail.com>
2026-03-10 23:55:13 -0400

    Update card-renderer.js

--- card-renderer.js
+++ card-renderer.js
@@ -1012,39 +1012,46 @@ function renderCardText(svgRoot, card) {
 // -----------------------------
 
 const testCard = {
-    "id": "crd_8bea9b8848964337b64e2ceed5c41302",
-    "name": "Hidden Inkcaster",
+    "id": "crd_58e889f440504f44b3283ed76f3f54a4",
+    "name": "Prince Eric",
+    "version": "Ursula's Groom",
     "layout": "normal",
     "released_at": "2024-05-17",
     "image_uris": {
       "digital": {
-        "small": "https://cards.lorcast.io/card/digital/small/crd_8bea9b8848964337b64e2ceed5c41302.avif?1716052430",
-        "normal": "https://cards.lorcast.io/card/digital/normal/crd_8bea9b8848964337b64e2ceed5c41302.avif?1716052430",
-        "large": "https://cards.lorcast.io/card/digital/large/crd_8bea9b8848964337b64e2ceed5c41302.avif?1716052430"
+        "small": "https://cards.lorcast.io/card/digital/small/crd_58e889f440504f44b3283ed76f3f54a4.avif?1716052430",
+        "normal": "https://cards.lorcast.io/card/digital/normal/crd_58e889f440504f44b3283ed76f3f54a4.avif?1716052430",
+        "large": "https://cards.lorcast.io/card/digital/large/crd_58e889f440504f44b3283ed76f3f54a4.avif?1716052430"
       }
     },
-    "cost": 2,
-    "inkwell": false,
-    "ink": "Emerald",
+    "cost": 6,
+    "inkwell": true,
+    "ink": "Amber",
     "inks": null,
     "type": [
-      "Item"
+      "Character"
     ],
-    "classifications": null,
-    "text": "FRESH INK When you play this item, draw a card.\nUNEXPECTED TREASURE All cards in your hand count as having {IW}.",
-    "keywords": [],
+    "classifications": [
+      "Floodborn",
+      "Hero",
+      "Prince"
+    ],
+    "text": "Shift 4 (You may pay 4 {I} to play this on top of one of your characters named Prince Eric.)\nUNDER VANESSA'S SPELL While you have a character named Ursula in play, this character gains Bodyguard and gets +2 {W}. (An opposing character who challenges one of your characters must choose one with Bodyguard if able.)",
+    "keywords": [
+      "Shift"
+    ],
     "move_cost": null,
-    "strength": null,
-    "willpower": null,
-    "lore": null,
-    "rarity": "Common",
+    "strength": 5,
+    "willpower": 5,
+    "lore": 2,
+    "rarity": "Uncommon",
     "illustrators": [
-      "Adam Fenton"
+      "Lisanne Koeteeuw"
     ],
-    "collector_number": "98",
+    "collector_number": "22",
     "lang": "en",
-    "flavor_text": "\"It looks like it's been here forever.\"\n-Flounder",
-    "tcgplayer_id": 549437,
+    "flavor_text": "",
+    "tcgplayer_id": 550561,
     "legalities": {
       "core": "legal"
     },
@@ -1055,7 +1062,7 @@ const testCard = {
     },
     "prices": {
       "usd": "0.07",
-      "usd_foil": "0.19"
+      "usd_foil": "0.28"
     }
   };
 
